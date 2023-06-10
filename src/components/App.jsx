388b45ee3d12c34d519664e37ba4ba4b48a119d7
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2023-06-10 14:59:40 +0200

    Add Friends list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Profile } from './Profile';
 import user from '../data/user.json';
-import data from '../data/data.json';
 import { Statistics } from './Statistics';
+import data from '../data/data.json';
+import { FriendList } from './FriendList';
+import friends from '../data/friends';
 
 export const App = () => {
   return (
@@ -15,6 +17,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles['friend-list']}>
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+const FriendListItem = ({ id, avatar, name, isOnline }) => {
+  const onlineStatus = isOnline ? styles.online : styles.offline;
+
+  return (
+    <li className={styles.item}>
+      <span className={`${styles.status} ${onlineStatus}`}></span>
+      <img
+        className={styles.avatar}
+        src={avatar}
+        alt="User avatar"
+        width="48"
+      />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+};
